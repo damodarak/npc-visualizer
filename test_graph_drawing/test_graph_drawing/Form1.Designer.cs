bb1c97b14115a53442790496c9f8da4f51bc2163
David Kroupa <damodarak@mail.com>
2023-05-27 13:43:45 +0200

    add testing graph module files

---
+++ test_graph_drawing/test_graph_drawing/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿
+namespace test_graph_drawing
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(780, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(133, 69);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Add Vertex";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(3, 3);
+            this.panel1.Name = "panel1";
+            this.tableLayoutPanel1.SetRowSpan(this.panel1, 4);
+            this.panel1.Size = new System.Drawing.Size(771, 544);
+            this.panel1.TabIndex = 1;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 84.54842F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 15.45158F));
+            this.tableLayoutPanel1.Controls.Add(this.button2, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.panel1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.button1, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.textBox1, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.textBox2, 1, 3);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(105, 40);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 45.65217F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 54.34783F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 31F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 334F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(919, 550);
+            this.tableLayoutPanel1.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(780, 87);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(133, 69);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Add Edge";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(780, 187);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(780, 218);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1067, 642);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ test_graph_drawing/test_graph_drawing/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Microsoft.Msagl.Drawing;
+
+namespace test_graph_drawing
+{
+    public partial class Form1 : Form
+    {
+        Graph g;
+        Microsoft.Msagl.GraphViewerGdi.GViewer viewer;
+        int counter = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            //create a viewer object 
+            viewer = new Microsoft.Msagl.GraphViewerGdi.GViewer();
+            //create a graph object 
+            g = new Graph("graph");
+            //create the graph content 
+            g.AddEdge("A", "B");
+            g.AddEdge("B", "A");
+            g.AddEdge("B", "C");
+            g.AddEdge("A", "C").Attr.Color = Microsoft.Msagl.Drawing.Color.Green;
+            g.FindNode("A").Attr.FillColor = Microsoft.Msagl.Drawing.Color.Magenta;
+            Node b = g.FindNode("B");
+            b.Attr.FillColor = Microsoft.Msagl.Drawing.Color.MistyRose;
+            Node c = g.FindNode("C");
+            c.Attr.FillColor = Microsoft.Msagl.Drawing.Color.PaleGreen;
+            c.Attr.Shape = Shape.Diamond;
+            //bind the graph to the viewer 
+            viewer.Graph = g;
+            //associate the viewer with the form 
+            this.SuspendLayout();
+            viewer.Dock = DockStyle.Fill;
+            this.panel1.Controls.Add(viewer);
+            this.ResumeLayout();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            g.AddNode((counter++).ToString());
+            viewer.Graph = g;
+            this.SuspendLayout();
+            this.panel1.Controls.Add(viewer);
+            this.ResumeLayout();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string t1 = textBox1.Text;
+            string t2 = textBox2.Text;
+
+            Node n1 = g.FindNode(t1);
+            Node n2 = g.FindNode(t2);
+
+            if(n1 != null && n2 != null)
+            {
+                g.AddEdge(t1, t2);
+                viewer.Graph = g;
+                this.SuspendLayout();
+                this.panel1.Controls.Add(viewer);
+                this.ResumeLayout();
+            }
+        }
+    }
+}
