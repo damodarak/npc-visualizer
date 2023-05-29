79d955ea286d3f21fc2fa7292e7c9a44bb244ab7
David Kroupa <damodarak@mail.com>
2023-05-29 12:43:32 +0200

    add specification template

--- test_sat_solver/test_sat_solver/Program.cs
+++ test_sat_solver/test_sat_solver/Program.cs
@@ -12,42 +12,72 @@ namespace test_sat_solver
     {
         static void Main(string[] args)
         {
+            //SatSolverParams parameters = new SatSolverParams();
+            //int limVar = 4;//nejvyssi literal v CNF formuli + 1
+            //Literal[][] clauses = new Literal[][]
+            //{
+            //new Literal[] { new Literal(1, true), new Literal(2, true), new Literal(3, true) },   // Clause 1: A or B or C
+            //new Literal[] { new Literal(1, false), new Literal(2, false), new Literal(3, true) },  // Clause 2: not A or not B or C
+            //new Literal[] { new Literal(1, false), new Literal(2, true), new Literal(3, false) },  // Clause 3: not A or B or not C
+            //new Literal[] { new Literal(1, true), new Literal(2, false), new Literal(3, false) }   // Clause 4: A or not B or not C
+            //};
+
+            //IEnumerable<SatSolution> solutions = SatSolver.Solve(parameters, limVar, clauses);
+
+            //bool hasSol = false;
+
+            //foreach (SatSolution solution in solutions)
+            //{
+            //    hasSol = true;
+            //    bool[] sol = new bool[3];
+            //    IEnumerable<Literal> lits = solution.Literals;
+            //    foreach (Literal lit in lits)
+            //    {
+            //        sol[lit.Var - 1] = lit.Sense;
+            //        //Console.WriteLine(lit.Sense);
+            //        //Console.WriteLine(lit.Var);
+            //    }               
+
+            //    Console.WriteLine($"Solution found: 1 = {sol[0]}, 2 = {sol[1]}, 3 = {sol[2]}");
+            //}
+
+            //if (!hasSol)
+            //{
+            //    Console.WriteLine("No solution found.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("There are solutions");
+            //}
+
             SatSolverParams parameters = new SatSolverParams();
-            int limVar = 4;//nejvyssi literal v CNF formuli + 1
+            int limVar = 5;//highest literal + 1
             Literal[][] clauses = new Literal[][]
             {
-            new Literal[] { new Literal(1, true), new Literal(2, true), new Literal(3, true) },   // Clause 1: A or B or C
-            new Literal[] { new Literal(1, false), new Literal(2, false), new Literal(3, true) },  // Clause 2: not A or not B or C
-            new Literal[] { new Literal(1, false), new Literal(2, true), new Literal(3, false) },  // Clause 3: not A or B or not C
-            new Literal[] { new Literal(1, true), new Literal(2, false), new Literal(3, false) }   // Clause 4: A or not B or not C
+                new Literal[] { new Literal(1, false), new Literal(2, false) },   // Clause 1: !1 or !2
+                new Literal[] { new Literal(1, false), new Literal(3, false) },   // Clause 2: !1 or !3
+                new Literal[] { new Literal(1, false), new Literal(4, false) },   // Clause 3: !1 or !4
+
+                new Literal[] { new Literal(2, false), new Literal(3, false) },   // Clause 4: !2 or !3
+                new Literal[] { new Literal(2, false), new Literal(4, false) },   // Clause 5: !2 or !4
+
+                new Literal[] { new Literal(3, false), new Literal(4, false) },   // Clause 6: !3 or !4
+
+                new Literal[] { new Literal(1, true), new Literal(2, true), new Literal(3, true), new Literal(4, true) } // Clause 7: 1 or 2 or 3 or 4
             };
 
             IEnumerable<SatSolution> solutions = SatSolver.Solve(parameters, limVar, clauses);
 
-            bool hasSol = false;
-
             foreach (SatSolution solution in solutions)
             {
-                hasSol = true;
-                bool[] sol = new bool[3];
+                bool[] sol = new bool[4];
                 IEnumerable<Literal> lits = solution.Literals;
                 foreach (Literal lit in lits)
                 {
                     sol[lit.Var - 1] = lit.Sense;
-                    //Console.WriteLine(lit.Sense);
-                    //Console.WriteLine(lit.Var);
-                }               
+                }
 
-                Console.WriteLine($"Solution found: 1 = {sol[0]}, 2 = {sol[1]}, 3 = {sol[2]}");
-            }
-
-            if (!hasSol)
-            {
-                Console.WriteLine("No solution found.");
-            }
-            else
-            {
-                Console.WriteLine("There are solutions");
+                Console.WriteLine($"Solution found: 1 = {sol[0]}, 2 = {sol[1]}, 3 = {sol[2]}, 4 = {sol[3]}");
             }
 
             Console.WriteLine("Press enter to close...");
