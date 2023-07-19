eba61b5b800762cf20297272f57e4ac0acc2b004
David Kroupa <damodarak@mail.com>
2023-07-19 12:31:55 +0200

    check existing edge before adding new one

--- npc-visualizer/npc-visualizer/Form1.Designer.cs
+++ npc-visualizer/npc-visualizer/Form1.Designer.cs
@@ -31,12 +31,12 @@ namespace npc_visualizer
         {
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
             this.button2 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.button3 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -45,7 +45,7 @@ namespace npc_visualizer
             this.tableLayoutPanel1.ColumnCount = 3;
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 85.80441F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 14.19558F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 137F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 138F));
             this.tableLayoutPanel1.Controls.Add(this.button1, 1, 0);
             this.tableLayoutPanel1.Controls.Add(this.panel1, 0, 0);
             this.tableLayoutPanel1.Controls.Add(this.button2, 2, 0);
@@ -73,15 +73,25 @@ namespace npc_visualizer
             this.button1.TabIndex = 0;
             this.button1.Text = "Add Vertex";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(3, 3);
+            this.panel1.Name = "panel1";
+            this.tableLayoutPanel1.SetRowSpan(this.panel1, 5);
+            this.panel1.Size = new System.Drawing.Size(791, 637);
+            this.panel1.TabIndex = 4;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(932, 3);
+            this.button2.Location = new System.Drawing.Point(931, 3);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(118, 70);
             this.button2.TabIndex = 1;
             this.button2.Text = "Add Edge";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // textBox1
             // 
@@ -93,19 +103,20 @@ namespace npc_visualizer
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(932, 84);
+            this.textBox2.Location = new System.Drawing.Point(931, 84);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(100, 20);
             this.textBox2.TabIndex = 3;
             this.textBox2.Text = "Second Vertex";
             // 
-            // panel1
+            // button3
             // 
-            this.panel1.Location = new System.Drawing.Point(3, 3);
-            this.panel1.Name = "panel1";
-            this.tableLayoutPanel1.SetRowSpan(this.panel1, 5);
-            this.panel1.Size = new System.Drawing.Size(791, 637);
-            this.panel1.TabIndex = 4;
+            this.button3.Location = new System.Drawing.Point(931, 170);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(118, 70);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Find solution";
+            this.button3.UseVisualStyleBackColor = true;
             // 
             // comboBox1
             // 
@@ -119,15 +130,6 @@ namespace npc_visualizer
             this.comboBox1.Size = new System.Drawing.Size(121, 21);
             this.comboBox1.TabIndex = 5;
             this.comboBox1.Text = "Select NPC problem";
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(932, 170);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(118, 70);
-            this.button3.TabIndex = 7;
-            this.button3.Text = "Find solution";
-            this.button3.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
--- npc-visualizer/npc-visualizer/Form1.cs
+++ npc-visualizer/npc-visualizer/Form1.cs
@@ -21,7 +21,17 @@ namespace npc_visualizer
         public Form1()
         {
             InitializeComponent();
+            init_graph_layout();
+        }
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            g.AddNode((counter++).ToString()).Attr.Shape = Shape.Circle;
+            viewer.Graph = g;
+        }
+
+        private void init_graph_layout()
+        {
             //create a viewer object 
             viewer = new Microsoft.Msagl.GraphViewerGdi.GViewer();
             viewer.EdgeInsertButtonVisible = false;
@@ -31,26 +41,39 @@ namespace npc_visualizer
 
             //create a graph object 
             g = new Graph("graph");
+
+            g.Directed = false;
+
             //create the graph content 
-            g.AddEdge("A", "B").Attr.ArrowheadAtTarget = ArrowStyle.None;
-            g.AddEdge("B", "C").Attr.ArrowheadAtTarget = ArrowStyle.None;
-            g.AddEdge("A", "C").Attr.ArrowheadAtTarget = ArrowStyle.None;
+            Edge e = g.AddEdge("-1", "-2");
+            e.Attr.ArrowheadAtTarget = ArrowStyle.None;
+            e.Attr.Id = "-1_-2";
 
-            int[] b = { 1, 2, 3 };
-            int eCount = g.EdgeCount;
-            IEnumerable<Edge> edges = g.Edges;
+            e = g.AddEdge("-2", "-3");   
+            e.Attr.ArrowheadAtTarget = ArrowStyle.None;
+            e.Attr.Id = "-2_-3";
 
-            foreach (var edge in edges)
-            {
-                Console.WriteLine($"{edge.Source} ---- {edge.Target}");
-            }
+            e = g.AddEdge("-1", "-3");
+            e.Attr.ArrowheadAtTarget = ArrowStyle.None;
+            e.Attr.Id = "-1_-3";
 
-            g.FindNode("A").Attr.FillColor = Microsoft.Msagl.Drawing.Color.Magenta;
-            g.FindNode("B").Attr.FillColor = Microsoft.Msagl.Drawing.Color.MistyRose;
+            //IEnumerable<Edge> edges = g.Edges;
 
-            Node c = g.FindNode("C");
-            c.Attr.FillColor = Microsoft.Msagl.Drawing.Color.PaleGreen;
-            c.Attr.Shape = Shape.Diamond;
+            //foreach (var edge in edges)
+            //{
+            //    Console.WriteLine($"{edge.Source} ---- {edge.Target}");
+            //}
+
+            //change vertex color
+            //g.FindNode("A").Attr.FillColor = Microsoft.Msagl.Drawing.Color.Magenta;
+            //g.FindNode("B").Attr.FillColor = Microsoft.Msagl.Drawing.Color.MistyRose;
+
+            Node c = g.FindNode("-3");
+            //c.Attr.FillColor = Microsoft.Msagl.Drawing.Color.PaleGreen;
+            c.Attr.Shape = Shape.Circle;
+            g.FindNode("-1").Attr.Shape = Shape.Circle;
+            g.FindNode("-2").Attr.Shape = Shape.Circle;
+
             //bind the graph to the viewer 
             viewer.Graph = g;
             //associate the viewer with the form 
@@ -58,6 +81,45 @@ namespace npc_visualizer
             viewer.Dock = DockStyle.Fill;
             this.panel1.Controls.Add(viewer);
             this.ResumeLayout();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string t1 = textBox1.Text;
+            string t2 = textBox2.Text;
+
+            Node n1 = g.FindNode(t1);
+            Node n2 = g.FindNode(t2);
+
+            Edge e1 = EdgeById(t1 + "_" + t2);
+            Edge e2 = EdgeById(t2 + "_" + t1);
+
+            if (n1 != null && n2 != null && e1 == null && e2 == null)
+            {
+                Edge new_e = g.AddEdge(t1, t2);
+                new_e.Attr.ArrowheadAtTarget = ArrowStyle.None;
+                new_e.Attr.Id = t1 + "_" + t2;
+
+                viewer.Graph = g;
+            }
+        }
+
+        private Edge EdgeById(string id)
+        {
+            IEnumerable<Edge> edges = g.Edges;
+
+            string[] vertices = id.Split('_');
+
+            foreach (Edge edge in edges)
+            {
+                if((edge.Source == vertices[0] && edge.Target == vertices[1]) || 
+                    (edge.Target == vertices[0] && edge.Source == vertices[1]))
+                {
+                    return edge;
+                }
+            }
+
+            return null;
         }
     }
 }
